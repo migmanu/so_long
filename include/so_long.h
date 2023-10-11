7b4fbb5b24ba393f239996310a5215178ea09d58
migmanu <jmigoya@gmail.com>
2023-10-11 19:28:39 +0200

    Update proto and formating

--- include/so_long.h
+++ include/so_long.h
@@ -6,7 +6,7 @@
 /*   By: migmanu <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/08 11:33:42 by migmanu           #+#    #+#             */
-/*   Updated: 2023/10/11 19:11:12 by migmanu          ###   ########.fr       */
+/*   Updated: 2023/10/11 19:27:26 by migmanu          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -65,15 +65,15 @@ typedef struct s_data
 #  define TRUE true
 # endif
 
-/*
- ***************	00_build_map	***************
- */
+/****************	00_build_map	****************/
 // build_map
 t_bool	build_map(t_data *data, char *file);
 
-/*
- ***************	10_error_handling	***************
- */
+/****************	01_parse_map	****************/
+// map_parse.c
+int	parse_map(t_dara *data);
+
+/****************	10_error_handling	****************/
 // handle_error.c
 void	handle_error(t_data *data, char str[]);
 void	exit_error(char str[]);
