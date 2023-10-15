d71d9a26095f2ff7c61a4244bf76a8e21cfb682e
migmanu <jmigoya@gmail.com>
2023-10-15 19:30:29 +0200

    Add wrong path error

--- src/10_error_handling/handle_error.c
+++ src/10_error_handling/handle_error.c
@@ -6,7 +6,7 @@
 /*   By: migmanu <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/11 19:07:38 by migmanu           #+#    #+#             */
-/*   Updated: 2023/10/11 20:42:30 by migmanu          ###   ########.fr       */
+/*   Updated: 2023/10/15 19:28:20 by migmanu          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,8 @@ void	exit_error(char str[])
 		ft_putendl_fd(WRGMAP, 1);
 	if (ft_strncmp(str, WRGWALL, ft_strlen(str)) == 0)
 		ft_putendl_fd(WRGWALL, 1);
+	if (ft_strncmp(str, NOPATH, ft_strlen(str)) == 0)
+		ft_putendl_fd(NOPATH, 1);
 	exit(ERROR);
 }
 
