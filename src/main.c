ec8730cd50baffc1b481c140b0994cc0f2cd62ab
Jose Manuel Migoya Bussolotti <jmigoya-@c4b12c1.42berlin.de>
2023-10-17 18:20:44 +0200

    Test image display

--- src/main.c
+++ src/main.c
@@ -6,46 +6,28 @@
 /*   By: migmanu <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/08 11:32:55 by migmanu           #+#    #+#             */
-/*   Updated: 2023/10/17 14:21:46 by migmanu          ###   ########.fr       */
+/*   Updated: 2023/10/17 17:18:30 by jmigoya-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/so_long.h"
 #include <stdint.h>
 #include <stdlib.h>
-#define WIDTH 256
-#define HEIGHT 256
+#define WIDTH 32
+#define HEIGHT 32
 #define BPP sizeof(int32_t)
-
-// Exit the program as failure.
-static void ft_error(void)
-{
-	fprintf(stderr, "%s", mlx_strerror(mlx_errno));
-	exit(EXIT_FAILURE);
-}
-
-static mlx_image_t	*g_img;
-// Print the window width and height.
-static void ft_hook(void* param)
-{
-	mlx_t *mlx;
-
-	mlx = param;
-	if (mlx_is_key_down(mlx, MLX_KEY_ESCAPE))
-		mlx_close_window(mlx);
-	if (mlx_is_key_down(mlx, MLX_KEY_P))
-		mlx_delete_image(mlx, g_img);
-	for (int x = 0; x < (int)g_img->width; x++)
-		for (int y = 0; y < (int)g_img->height; y++)
-			mlx_put_pixel(g_img, x, y, rand() % RAND_MAX);
-
-}
 
 void	keyhook(mlx_key_data_t keydata, void *param)
 {
 	(void )param;
 	if (keydata.key == MLX_KEY_W && keydata.action == MLX_PRESS)
 		puts("Hola mundo!");
+}
+
+static void error(void)
+{
+	puts(mlx_strerror(mlx_errno));
+	exit(EXIT_FAILURE);
 }
 
 int	main(int argc, char **argv)
@@ -58,32 +40,43 @@ int	main(int argc, char **argv)
 	if (flood_map(data) == -1)
 		handle_error(&data, NOPATH);
 
-// MLX allows you to define its core behaviour before startup.
-	mlx_set_setting(MLX_MAXIMIZED, true);
-	mlx_t* mlx = mlx_init(WIDTH, HEIGHT, "42Balls", true);
+	// Start mlx
+	mlx_t* mlx = mlx_init((WIDTH * data.map.line_length), (HEIGHT * data.map.line_count), "Test", true);
 	if (!mlx)
-		ft_error();
+        	error();
 
-	/* Do stuff */
+	// Try to load the file
+	mlx_texture_t* texture = mlx_load_png("./assets/StoneBrickFloor.png");
+	if (!texture)
+        	error();
 
-	// Create and display the image.
-	mlx_image_t* img = mlx_new_image(mlx, 256, 256);
-	if (!img || (mlx_image_to_window(mlx, img, 0, 0) < 0))
-		ft_error();
+	// Convert texture to a displayable image
+	mlx_image_t* img = mlx_texture_to_image(mlx, texture);
+	if (!img)
+        	error();
 
-	// Even after the image is being displayed, we can still modify the buffer.
-	// mlx_put_pixel(img, 100, 100, 0xFF0000FF);
-	// memset(img->pixels, 255, img->width * img->height * BPP);
+	// Display the image
+	printf("about to display lin %d, col %zu\n", data.map.line_count, data.map.line_length);
+	int	lin = 0;
+	int	col;
+	while (lin < (int)data.map.line_count)
+	{
+		col = 0;
+		while (col < (int)data.map.line_length)
+		{
+			mlx_image_to_window(mlx, img, (HEIGHT *col), (WIDTH *lin));
+			col++;
+		}
+		lin++;
+	}
+	printf("lin %d, col %d\n", lin, col);
 
-	// Register a hook and pass mlx as an optional param.
-	// NOTE: Do this before calling mlx_loop!
-	mlx_key_hook(mlx, &keyhook, NULL);
-	g_img = mlx_new_image(mlx, 128, 128);
-	mlx_image_to_window(mlx, g_img, 0, 0);
-	mlx_loop_hook(mlx, ft_hook, mlx);
 	mlx_loop(mlx);
-	mlx_terminate(mlx);
 
-	ft_free_vec(data.map.map_v);
-	return (0);
+	mlx_delete_image(mlx, img);
+	mlx_delete_texture(texture);
+
+        // Optional, terminate will clean up any leftover images (not textures!)
+	mlx_terminate(mlx);
+	return (EXIT_SUCCESS);
 }
