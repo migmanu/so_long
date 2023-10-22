1c87f240b96e93ae5b5c0a398adf3c11a445c87f
migmanu <jmigoya@gmail.com>
2023-10-22 18:32:32 +0200

    Add player up movement!

--- src/03_move_character/move_character.c
+++ src/03_move_character/move_character.c
@@ -6,7 +6,7 @@
 /*   By: migmanu <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/22 17:33:11 by migmanu           #+#    #+#             */
-/*   Updated: 2023/10/22 18:05:41 by migmanu          ###   ########.fr       */
+/*   Updated: 2023/10/22 18:32:02 by migmanu          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,15 @@
 
 void	move_up(t_data *data)
 {
+	printf("move_up init\n");
 	if (data->map.map_v[data->player_pos_y - 1][data->player_pos_x] != '1')
 	{
+		data->img.dora_1->instances[0].y -= 1 * SIZE;
 		data->player_pos_y -= 1;
+		if (data->map.map_v[data->player_pos_y][data->player_pos_x] == '1')
+		{
+			data->map.coll -= 1;
+		}
 	}
 }
 
@@ -25,6 +31,6 @@ void	move_hook(mlx_key_data_t keydata, void *data)
 	if (mlx_is_key_down(((t_data *)data)->mlx, MLX_KEY_ESCAPE))
 		mlx_close_window(((t_data *)data)->mlx);
 	if (keydata.key == MLX_KEY_W && keydata.action == MLX_PRESS)
-		printf("w\n");
+		move_up(data);
 }
 
