8f76981b124043f2f861a9b64efbef050460caab
migmanu <jmigoya@gmail.com>
2023-10-23 11:54:59 +0200

    Add remove_coll()

--- src/03_move_character/move_character.c
+++ src/03_move_character/move_character.c
@@ -6,7 +6,7 @@
 /*   By: migmanu <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/22 17:33:11 by migmanu           #+#    #+#             */
-/*   Updated: 2023/10/22 23:00:38 by migmanu          ###   ########.fr       */
+/*   Updated: 2023/10/23 11:53:57 by migmanu          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,20 @@ void	print_moves(t_data *data)
 	printf("%d\n", data->moves); // change for my ft_printf
 }
 
+void	remove_coll(t_data *data, int x, int y)
+{
+	mlx_image_to_window(
+		data->mlx,
+		data->img.floor_1,
+		(x * SIZE),
+		(y * SIZE));
+	data->map.map_v[y][x] = '0';
+}
+
 void	check_game_status(t_data *data)
 {
+	if (data->map.map_v[data->player_pos_y][data->player_pos_x] == 'C')
+		remove_coll(data, data->player_pos_x, data->player_pos_y);
 	if (data->map.coll == 0)
 	{
 		printf("end game\n");
